3304ef8b45a41b43ca972cc4f31cfd39e519fc59
Casey Preston <caseypreston4@gmail.com>
2020-06-26 13:11:34 -0600

    video 1 and 2

---
+++ varibles-and-data.js
@@ -0,0 +1,106 @@
+//variables, Data types, and typing
+//
+//let wordData ="word Data";
+//
+// console.log(wordData);
+//
+// let subTotal = 100;
+// let tax = x * 0.07;
+// let totalWithTax = tax + subTotal;
+//
+//
+// //Data Types
+//
+//let obj ={
+//    key1: "value",
+//    key2: 4,
+//    //boolean: true,
+//  // obj2:{
+//       // obj2Key1: "value of inner object",
+//    },
+//};
+//
+////let var1 = "2"
+////let var2 =5;
+////let results = var1 + var2
+//
+//let add= 1+1;
+//let sub = 2-2;
+//let mult = 2*4;
+//let division =4/2
+//let mod = 5 % 2;
+//let string = "String 1 " +"String 2"
+//
+////Conditional and Functions
+// pieces of code that evaluate true or false
+////Equals
+//let equals = 1 === 1;
+//console.log(equals);
+//
+////Greater than
+//let GreaterThan = 2 > 10;
+//
+////Less than
+//let lessThan = 2 < 10;
+//
+////Greater than Equals
+//let greaterThanEq = 5 >= 5;
+//
+////Less than Equals
+//let lessThanEq = 4 <= 9;
+//
+////Not Equals
+//let notEquals = 5 !== 2;
+
+//let storeA = 3.10;
+//let storeB = 4.40;
+
+//let storeAIsLower = storeA < storeB;
+
+//function compareStorePrices(storeA, storeB) {
+
+   // if (storeAIsLower) {
+     //   console.log("Store A has a Lower price.")
+   // } else if (storeB < storeA) {
+    //    console.log("Store B has A lower price.")
+   // } else {
+    //    console.log("Their prices are equal.")
+   // }
+//}
+
+ //compareStorePrices (10, 5);
+// compareStorePrices(7, 10);
+ 
+ //function squareNum (number) {
+   // return number * number;
+    
+
+ //}
+//let squaredNumber = squareNum(8);
+//console.log(squaredNumber);
+
+//let x = 10;
+
+//function addNumbers(n,m, x) {
+  //  let b;
+    //if (1===1){
+   //     b = 8;
+   // }
+    //return n + m;
+
+//}
+ //addNumbers(2,3, 10);
+//             0, 1, 2, 3, 4
+//let ourArray =[1, 2, 3, 4, 5];
+//let arrLey = ourArray.length;
+//for (let i = 0; i<arrLey; i++){
+//    console.log("i is equal to:" +i);
+//console.log(ourArray[i])
+   // for(let j =0; j<10; j++){
+   // console.log("j is equal to:" + j);
+  //  }
+//
+ while ( x < 10){
+    console.log('ran');
+    x = x + 1;
+ }
